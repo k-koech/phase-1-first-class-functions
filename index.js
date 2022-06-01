59f6c2d885cb598509e8d4684b49ec0ce0eef188
k-koech <kelvin.koech@student.moringaschool.com>
2022-06-01 14:47:43 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+
+function receivesAFunction(callback)
+{
+    return callback();
+}
+
+const x = function x()
+ { 
+     return "Stretch! Work that core!"; 
+ }
+
+receivesAFunction(x);
+
+
+const anonymousFxn = function y() {
+    return 'Stretch! Work that core!';
+ }
+function returnsANamedFunction()
+{
+    return anonymousFxn;
+}
+
+
+function returnsAnAnonymousFunction()
+{
+    return function(){return "abc";}
+}
+
+
+
+//  returnsANamedFunction(anonymousFxn);
+
+
+
+
+
+
+
